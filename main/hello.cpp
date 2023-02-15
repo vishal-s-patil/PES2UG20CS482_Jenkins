497e0761a6d0a0e169851474c1f5c3d21a6046a6
Vishal Patil <78012271+vishal-s-patil@users.noreply.github.com>
2023-02-16 01:19:54 +0530

    PES2UG20CS482

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "srn : PES2UG20CS482\n";
+  return 0;
+}
