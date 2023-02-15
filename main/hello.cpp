5bfba0394849f3489dd495c543b0dbb5838f4464
Vishal Patil <78012271+vishal-s-patil@users.noreply.github.com>
2023-02-16 02:19:04 +0530

    changes made

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout << "srn : PES2UG20CS482\n";
-  cout << "changes made\n";
+  cout << "changes made!\n";
   return 0;
 }
